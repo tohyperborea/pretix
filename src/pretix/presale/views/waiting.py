174a0f51faf4b723901cb59f3b82b988b20984ea
S. Omar Kazmi <omarkazmi@gmail.com>
2022-12-05 18:28:50 -0500

    Require customer to be logged in to access the waiting list page.

--- src/pretix/presale/views/waiting.py
+++ src/pretix/presale/views/waiting.py
@@ -34,6 +34,7 @@ from pretix.base.models import Quota, SubEvent
 from pretix.base.templatetags.urlreplace import url_replace
 from pretix.multidomain.urlreverse import eventreverse
 from pretix.presale.views import EventViewMixin
+from pretix.presale.views.customer import CustomerRequiredMixin
 
 from ...base.i18n import get_language_without_region
 from ...base.models import Voucher, WaitingListEntry
@@ -42,7 +43,7 @@ from . import allow_frame_if_namespaced
 
 
 @method_decorator(allow_frame_if_namespaced, 'dispatch')
-class WaitingView(EventViewMixin, FormView):
+class WaitingView(EventViewMixin, CustomerRequiredMixin, FormView):
     template_name = 'pretixpresale/event/waitinglist.html'
     form_class = WaitingListForm
 
@@ -137,7 +138,7 @@ class WaitingView(EventViewMixin, FormView):
 
 
 @method_decorator(allow_frame_if_namespaced, 'dispatch')
-class WaitingRemoveView(EventViewMixin, TemplateView):
+class WaitingRemoveView(EventViewMixin, CustomerRequiredMixin, TemplateView):
     template_name = 'pretixpresale/event/waitinglist_remove.html'
 
     def get_context_data(self, **kwargs):
